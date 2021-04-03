201344efe9e24678b8447f0e1e923d60af0ce372
Modupe Adebayo <madebayo@systemspecs.com.ng>
2021-04-03 20:47:45 +0100

    add header to homepage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { Route, Switch } from "react-router-dom";
 import "./App.css";
+import { Header } from "./components/header/Header";
 import { HomePage } from "./pages/home-page/HomePage";
 import ShopPage from "./pages/shop/Shop";
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route exact path="/shop" component={ShopPage} />
