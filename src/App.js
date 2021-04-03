2592d0c896b51a05f90c9dee3ef0b6db262ee350
Modupe Adebayo <madebayo@systemspecs.com.ng>
2021-04-03 20:32:55 +0100

    updating the app with a collection item component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import { Route, Switch } from "react-router-dom";
 import "./App.css";
 import { HomePage } from "./pages/home-page/HomePage";
+import ShopPage from "./pages/shop/Shop";
 
 function App() {
   return (
-    <div className="App">
-      <HomePage />
+    <div>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/shop" component={ShopPage} />
+      </Switch>
     </div>
   );
 }
---
+++ src/components/preview-collection-item/PreviewCollectionItem.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./PreviewCollectionItem.scss";
+
+export const PreviewCollectionItem = ({ id, name, price, imageUrl }) => {
+  return (
+    <div className="collection-item">
+      <div className="image" style={{ backgroundImage: `url(${imageUrl})` }}></div>
+      <div className="collection-footer">
+        <span className="name">{name}</span>
+        <span className="price">{price}</span>
+      </div>
+    </div>
+  );
+};
